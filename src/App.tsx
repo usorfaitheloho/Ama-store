4fbd82e17a7ae0ddae7a47953defe96faa861e0a
usorfaitheloho <usorfaitheloho@gmail.com>
2025-10-09 14:49:38 +0300

    Add routes for main content in app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import {BrowserRouter as Router }from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
 
-export default function App () {
-return <Router>
-  <div className="flex h-screen">
-  <Sidebar />
-  </div>
-</Router>
+export default function App() {
+  return (
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex flex-wrap justify-between w-full rounded">
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
